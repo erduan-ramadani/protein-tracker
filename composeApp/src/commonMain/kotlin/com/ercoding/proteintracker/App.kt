e41e651a44c1b2ba93ee4dce655c10a76448a67e
Erduan Ramadani <ramadani.erduan@web.de>
2026-04-11 12:16:23 +0200

    WIP: add basic UI with progress bar, text input and button

--- composeApp/src/commonMain/kotlin/com/ercoding/proteintracker/App.kt
+++ composeApp/src/commonMain/kotlin/com/ercoding/proteintracker/App.kt
@@ -1,48 +1,63 @@
 package com.ercoding.proteintracker
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import org.jetbrains.compose.resources.painterResource
-
-import proteintracker.composeapp.generated.resources.Res
-import proteintracker.composeapp.generated.resources.compose_multiplatform
+import androidx.compose.ui.unit.dp
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+
+        var userTextInput by remember { mutableStateOf("") }
+        var consumedProtein by remember { mutableStateOf(0.75f) }
+
         Column(
             modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+                .fillMaxSize()
+                .background(Color.White)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+            Text(text = "ProteinTracker", color = Color.Black)
+            Spacer(modifier = Modifier.padding(8.dp))
+            LinearProgressIndicator(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                progress = { consumedProtein }
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+            OutlinedTextField(
+                value = userTextInput,
+                onValueChange = { userTextInput = it },
+                label = { Text("Was hast du heute gegessen?") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+            Button(
+                onClick = {},
+                enabled = true,
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                Text("Proteine hinzufügen")
             }
         }
     }
--- composeApp/src/commonMain/kotlin/com/ercoding/proteintracker/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.ercoding.proteintracker
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
